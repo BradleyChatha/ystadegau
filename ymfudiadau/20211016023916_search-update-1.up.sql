1d5ec334daf15ce235676f330f5177d324ab9ce3
Bradley Chatha <bradley@chatha.dev>
2021-10-16 04:03:56 +0100

    Improved search function

---
+++ ymfudiadau/20211016023916_search-update-1.up.sql
@@ -0,0 +1,36 @@
+CREATE OR FUNCTION search_packages(in query text) RETURNS TABLE(id int, name text, rank real)
+AS $$
+    SELECT DISTINCT ON (id)
+        id, name, SUM(rank) AS rank
+    FROM
+    (
+        SELECT 
+            id, name, 10 AS rank 
+        FROM 
+            package 
+        WHERE 
+            name = query
+        UNION ALL
+        (
+            SELECT
+                id, name, 1 AS rank
+            FROM
+                package
+            WHERE
+                name LIKE (query || '%')
+                OR
+                name LIKE ('%' || query)
+        )
+        UNION ALL
+        (
+            SELECT 
+                id, name, ts_rank_cd(query_vector, to_tsquery(query)) AS rank 
+            FROM 
+                package 
+            WHERE 
+                query_vector @@ to_tsquery(query)
+        )
+    ) AS _
+    GROUP BY id, name;
+$$
+LANGUAGE SQL;
